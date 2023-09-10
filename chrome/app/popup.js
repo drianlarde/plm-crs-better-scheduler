ab473b229f3f9a0be7cd0ac3e119595bda6a199c
drianlarde <aadabelarde2020@plm.edu.ph>
2023-09-10 18:56:34 +0800

    [Feature][Adrian] Add .csv to .ics file conversion for Apple Calendar and Google Calendar

--- chrome/app/popup.js
+++ chrome/app/popup.js
@@ -1,4 +1,4 @@
-document.getElementById("convertButton").addEventListener("click", () => {
+document.getElementById("convertCSVButton").addEventListener("click", () => {
   let startDate = document.getElementById("startDate").value;
   let endDate = document.getElementById("endDate").value;
 
@@ -60,4 +60,149 @@ document.getElementById("deleteAllEvents").addEventListener("click", () => {
       }
     }
   });
+});
+
+document.getElementById("fileUploadCSV").addEventListener("change", () => {
+  const file = document.getElementById("fileUploadCSV").files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    const rows = e.target.result.split("\n");
+
+    let icsContent = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Chimbori//CSV to iCal Convertor// https://csv-to-ical.chimbori.com/ //EN
+CALSCALE:GREGORIAN
+X-WR-CALNAME;VALUE=TEXT:schedule (35)\n`;
+
+    rows.forEach((row, index) => {
+      if (index === 0 || row.trim() === "") {
+        // Skip header row or empty rows
+        return;
+      }
+
+      /*
+      Sample Desired Output:
+      SUMMARY:🖥 | CS Thesis Writing 1 - CSC 0411 1 LecSyncOnline
+DTSTAMP:20230904T070000
+DESCRIPTION:CS Thesis Writing 1 - CSC 0411 1
+DTSTART:20230904T070000
+DTEND:20230904T083000
+LOCATION:GCA 306
+END:VEVENT
+      */
+
+      /*
+      columns
+[
+    "🖥 | CS Thesis Writing 1 - CSC 0411 1 LecSyncOnline",
+    "2023-09-04",
+    "7:00 AM",
+    "2023-09-04",
+    "8:30 AM",
+    "FALSE",
+    "CS Thesis Writing 1 - CSC 0411 1",
+    "GCA 306",
+    "TRUE\r"
+] 
+      */
+      const columns = row.split(",");
+
+      if (columns.length < 5) {
+        // Skip rows with fewer than 5 columns
+        console.warn(
+          `Skipping row ${index} because it has fewer than 5 columns`
+        );
+        return;
+      }
+
+      // "TRUE"
+      const allDayEvent = columns[5] === "TRUE" ? true : false;
+
+      // "CS Thesis Writing 1 - CSC 0411 1"
+      const description = columns[6];
+
+      // "GCA 306"
+      const location = columns[7];
+
+      // "🖥 | CS Thesis Writing 1 - CSC 0411 1 LecSyncOnline"
+      const subject = columns[0];
+
+      console.log({ columns, subject, description, location, allDayEvent });
+
+      // "AM"
+      let endAmPm = columns[4].split(" ")[1];
+
+      // "20230904"
+      let endDate = columns[3].replace(/-/g, "");
+
+      // "83000"
+      let endTime = columns[4].split(" ")[0].replace(/:/g, "") + "00";
+
+      // "AM"
+      let startAmPm = columns[2].split(" ")[1];
+
+      // "20230904"
+      let startDate = columns[1].replace(/-/g, "");
+
+      // "70000"
+      let startTime = columns[2].split(" ")[0].replace(/:/g, "") + "00";
+
+      console.log({
+        startDate,
+        startTime,
+        startAmPm,
+        endDate,
+        endTime,
+        endAmPm,
+      });
+
+      if (startAmPm === "PM") {
+        let startHour = parseInt(startTime.substring(0, 2));
+        if (startHour !== 12) {
+          startHour += 12;
+        }
+        startTime = startHour.toString() + startTime.substring(2);
+      }
+
+      if (endAmPm === "PM") {
+        let endHour = parseInt(endTime.substring(0, 2));
+        if (endHour !== 12) {
+          endHour += 12;
+        }
+        endTime = endHour.toString() + endTime.substring(2);
+      }
+
+      // Incorrect: DTSTAMP:20230904T70000
+      // Corrected: DTSTAMP:20230904T070000
+      startTime = startTime.padStart(6, "0");
+      endTime = endTime.padStart(6, "0");
+
+      if (allDayEvent) {
+        startTime = "";
+        endTime = "";
+      }
+
+      icsContent += `BEGIN:VEVENT
+SUMMARY:${subject}
+DTSTAMP:${startDate}T${startTime}
+DESCRIPTION:${description}
+DTSTART:${startDate}T${startTime}
+DTEND:${endDate}T${endTime}
+LOCATION:${location}
+END:VEVENT\n`;
+    });
+
+    icsContent += `END:VCALENDAR`;
+
+    // Create a link to download the .ics file
+    const hiddenElement = document.createElement("a");
+    hiddenElement.href =
+      "data:text/calendar;charset=utf-8," + encodeURI(icsContent);
+    hiddenElement.target = "_blank";
+    hiddenElement.download = "schedule.ics";
+    hiddenElement.click();
+  };
+
+  reader.readAsText(file);
 });
